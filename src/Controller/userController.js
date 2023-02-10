497586ed03c46fe91158dab55838fe40963f1d8d
BrunoSouza <brunowebsouza@gmail.com>
2023-02-10 16:40:06 -0300

    create and update running

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -6,55 +6,64 @@ class UserController{
 
     async create (request,response){
          const {name,email,password}= request.body
-         const UserWhithSameEmail= await knex("users").where({email}).first()
-         if(UserWhithSameEmail){
+
+         const UserWhithSameEmail= await knex('users').where({email})
+         if(UserWhithSameEmail.length===1){
             throw new  AppError("Este email já está em uso")
           
-         }
-       
-            const hashedPassword= await hash(password,8)
-            await knex("users").insert({name:name,email:email,password:hashedPassword})
+         }else{
+          const hashedPassword= await hash(password,8)
+            await knex("users").insert({name,email,password:hashedPassword})
             response.status(201).json({
               message:`Usuário (a) ${name} cadastrado com sucesso!`})
     
+         }
         
     }
     async update(request,response){
         const {name,email,oldpassword,password}= request.body
         const {id}= request.params
         
-        
         const User= await knex("users").where({id})
+        const UserWhithSameEmail= await knex("users").where({email})
 
-        if(User.length==0){
+        if(User.length===0){
             throw new AppError('Usuário não encontrado')
         }
-        const UserWhithSameEmail= await knex("users").where({email})
-        if(  UserWhithSameEmail.id !== User.id ){
+        
+        if( UserWhithSameEmail[0] && UserWhithSameEmail[0].id !== User[0].id ){
             throw new AppError('Este e-mail já está cadastrado')
         }
         
-          if(password && !oldpassword){
-            throw new AppError('Informe a senha antiga')
-          }
-          if(password && oldpassword){
-            const CheckOldPassword= await compare(oldpassword,User.password)
-            if(!CheckOldPassword){
-                throw new AppError('Email e/ou senha incorretos',401)
-            }
-          }
+        if(password && !oldpassword){
+          throw new AppError('Informe a senha antiga')
+        }
+        if(password && oldpassword){
+           const CheckOldPassword= await compare(oldpassword,User[0].password)
+
+           if(!CheckOldPassword){
+            throw new AppError('Senha incorreta')
+           }else{
+            User[0].name= name ?? User[0].name
+            User[0].email=email ?? User[0].email
+            const hashedPassword=await hash(password,8)
+            User[0].password=hashedPassword ?? User[0].password
+            
+            await knex('users').update({name:User[0].name, email:User[0].email, password:User[0].password}).where({id})
+            response.status(200).json({message:`Dados do(a) usuário(a) ${name} atualizados`})
+           }
+        }
+       
+              
+        
+            
+
+        }
         
 
        
-        User.name= name ?? User.name
-        User.email=email ?? User.email
-        const hashedPassword=await hash(password,8)
-        User.password=hashedPassword ?? User.password
-        
-        await knex('users').update({name:User.name, email:User.email, password:User.password}).where({id})
-        response.status(200).json({message:`Dados do usuário(a) ${name} atualizados`})
     }
 
-}
+
 
 module.exports= UserController
